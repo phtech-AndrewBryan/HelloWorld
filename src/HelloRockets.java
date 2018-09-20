587ad1bf7b50f3468a04b0c5271c7b4889855d50
Drew Bryan <andrew.bryan@phtech.com>
2018-09-20 11:26:47 -0700

    Added Rcokets

---
+++ src/HelloRockets.java
@@ -0,0 +1,70 @@
+public class HelloRockets {
+	private static void DrawTopCone() {
+		System.out.print("   /\\   ");
+	}
+	private static void DrawMiddleCone() {
+		System.out.print("  /  \\  ");
+	}
+	private static void DrawBottomCone() {
+		System.out.print(" /    \\ ");
+	}
+	
+	private static void Draw2Cones() {
+		DrawTopCone();
+		System.out.print(" ");
+		DrawTopCone();
+		System.out.println();
+		
+		DrawMiddleCone();
+		System.out.print(" ");
+		DrawMiddleCone();
+		System.out.println();
+		
+		DrawBottomCone();
+		System.out.print(" ");
+		DrawBottomCone();
+		System.out.println();
+	}
+	
+	private static void DrawHorizontal() {
+		System.out.print("+------+");
+	}
+	private static void DrawVertical() {
+		System.out.print("|      |");
+	}
+	
+	private static void Draw2Horizontals() {
+		DrawHorizontal();
+		System.out.print(" ");
+		DrawHorizontal();
+		System.out.println();
+	}
+	
+	private static void Draw2Verticals() {
+		DrawVertical();
+		System.out.print(" ");
+		DrawVertical();
+		System.out.println();
+	}
+	private static void Draw2Centers() {
+		Draw2Horizontals();
+		Draw2Verticals();
+		Draw2Verticals();
+		Draw2Horizontals();
+		
+		System.out.println("|United| |United|");
+		System.out.println("|States| |States|");
+		
+		Draw2Horizontals();
+		Draw2Verticals();
+		Draw2Verticals();
+		Draw2Horizontals();
+		
+	}
+	
+	public static void main(String[] args) {
+		Draw2Cones();
+		Draw2Centers();
+		Draw2Cones();
+	}
+}
